17743bd3847cd50bb032cec02273b1e69ac429f6
zappaboy <federico_zappone@hotmail.it>
2022-01-12 00:58:20 +0100

    Changed auth method;

--- app/services/twitter.py
+++ app/services/twitter.py
@@ -8,7 +8,11 @@ class Twitter:
     def __init__(self):
         self.logger = Logger('Twitter')
         configurations = get_twitter_configurations()
-        self.client = tweepy.Client(bearer_token=configurations['BEARER_TOKEN'])
+        self.client = tweepy.Client(
+            consumer_key=configurations['CONSUMER_KEY'],
+            consumer_secret=configurations['CONSUMER_KEY_SECRET'],
+            access_token=configurations['ACCESS_TOKEN'],
+            access_token_secret=configurations['ACCESS_TOKEN_SECRET'])
 
     def test(self):
         return self.search(keyword='Tweepy', limit=2)
@@ -18,8 +22,9 @@ class Twitter:
         query = keyword + ' -is:retweet lang:en'
         tweets = self.client.search_recent_tweets(
             query=query,
-            tweet_fields=['text', 'id'],
+            tweet_fields=['text', 'id', 'public_metrics'],
             max_results=limit,
             start_time=start_time,
-            end_time=end_time)
+            end_time=end_time,
+            user_auth=True)
         return tweets.data
--- app/utils/configurator.py
+++ app/utils/configurator.py
@@ -3,9 +3,11 @@ from os import environ
 
 def get_twitter_configurations():
     twitter_credentials = {
-        'API_KEY': environ.get('API_KEY'),
-        'API_KEY_SECRET': environ.get('API_KEY_SECRET'),
+        'CONSUMER_KEY': environ.get('CONSUMER_KEY'),
+        'CONSUMER_KEY_SECRET': environ.get('CONSUMER_KEY_SECRET'),
         'BEARER_TOKEN': environ.get('BEARER_TOKEN'),
+        'ACCESS_TOKEN': environ.get('ACCESS_TOKEN'),
+        'ACCESS_TOKEN_SECRET': environ.get('ACCESS_TOKEN_SECRET'),
     }
 
     return twitter_credentials
