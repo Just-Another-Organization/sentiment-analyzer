9667b3bb0cc3fc1a90cf9ed991c68b9cd1938416
zappaboy <federico_zappone@hotmail.it>
2022-01-15 00:10:49 +0100

    Added redirect on static contents;

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
-import api
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from starlette.responses import RedirectResponse
+
+import api
 from utils.list_string_flattening_middleware import QueryStringFlatteningMiddleware
 
 load_dotenv()
+
+STATIC_FILES_PATH = "/engine"
 
 # Doc: http://127.0.0.1:80/redoc
 app = FastAPI(
@@ -14,9 +18,15 @@ app = FastAPI(
 )
 
 app.add_middleware(QueryStringFlatteningMiddleware)
-app.mount("/engine", StaticFiles(directory="static", html=True), name="static")
+app.mount(STATIC_FILES_PATH, StaticFiles(directory="static", html=True), name="static")
 
 app.include_router(
     api.router,
     prefix="/api",
     tags=["api"])
+
+
+@app.get("/")
+async def redirect():
+    response = RedirectResponse(url=STATIC_FILES_PATH)
+    return response
