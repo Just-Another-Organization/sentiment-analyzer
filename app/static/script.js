4d38b3e94d78ccce22d57fec1fc3e944910d29d0
zappaboy <federico_zappone@hotmail.it>
2022-01-14 03:50:43 +0100

    Added timeframe buttons to static page;

--- app/static/script.js
+++ app/static/script.js
@@ -1,3 +1,5 @@
+const BASE_URL = window.location.origin + '/api'
+
 RECENT_MODE = 'recent'
 POPULAR_MODE = 'popular'
 
@@ -6,11 +8,27 @@ MODES = [
     POPULAR_MODE
 ]
 
+ONE_HOUR = '1h'
+FOUR_HOUR = '4h'
+ONE_DAY = '1d'
+THREE_DAYS = '3d'
+
+TIMEFRAMES = [
+    ONE_HOUR,
+    FOUR_HOUR,
+    ONE_DAY,
+    THREE_DAYS
+]
+
 let currentMode = RECENT_MODE
 const MODES_NUMBER = MODES.length;
+let currentTimeframe = ONE_HOUR
+
+let timeframeListElement
 let modeElement
 let inputSearchElement
-const BASE_URL = window.location.origin + '/api'
+let timeframeButton
+let timeFrameShowed = false
 
 function submitSearch() {
     const input = inputSearchElement.value
@@ -32,17 +50,12 @@ function changeMode() {
 function setMode(modeName) {
     currentMode = modeName
     setModeText(modeName)
+    checkModeOptions()
     animate()
 }
 
 function setModeText(text) {
     modeElement.textContent = text;
-}
-
-function init() {
-    modeElement = document.getElementById('search-mode')
-    inputSearchElement = document.getElementById('search')
-    setMode(currentMode)
 }
 
 function animate() {
@@ -57,4 +70,52 @@ function animate() {
             duration: 4000,
             delay: (el, i) => 80 * (i + 1)
         })
+}
+
+function setTimeframe(timeframe) {
+    currentTimeframe = timeframe;
+    showTimeframes()
+}
+
+function showTimeframes() {
+    if (timeFrameShowed) {
+        timeframeListElement.style.display = 'none';
+    } else {
+        timeframeListElement.style.display = 'block';
+        timeframeListElement.innerHTML = '';
+        for (const timeframe of TIMEFRAMES) {
+            const li = document.createElement("li");
+            li.appendChild(document.createTextNode(timeframe));
+            li.onclick = () => setTimeframe(timeframe)
+            timeframeListElement.appendChild(li);
+        }
+    }
+    timeFrameShowed = !timeFrameShowed;
+}
+
+function checkModeOptions() {
+    if (inputSearchElement.value.length > 0) {
+        if (currentMode === RECENT_MODE) {
+            inputSearchElement.style.width = '78rem';
+            timeframeButton.style.display = 'block';
+        } else {
+            inputSearchElement.style.width = '84rem';
+            timeframeButton.style.display = 'none';
+        }
+    } else {
+        inputSearchElement.style.width = '90rem';
+        timeframeButton.style.display = 'none';
+    }
+}
+
+function checkInput() {
+    checkModeOptions()
+}
+
+function init() {
+    modeElement = document.getElementById('search-mode')
+    inputSearchElement = document.getElementById('search')
+    timeframeListElement = document.getElementById('timeframes-list')
+    timeframeButton = document.getElementById('timeframe-btn')
+    setMode(currentMode)
 }
