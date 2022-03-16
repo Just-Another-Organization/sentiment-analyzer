b36b3e64cc233f7ca78d66215bb12a52d3bf92b9
zappaboy <federico_zappone@hotmail.it>
2022-03-16 01:22:31 +0100

    Refactoring; Changed available timeframes;

--- app/utils/utilities.py
+++ app/utils/utilities.py
@@ -19,7 +19,7 @@ def get_sentiment_by_label(label):
     return constants.LABELS[label]
 
 
-def get_interval_by_string(timeframe):
+def get_seconds_by_timeframe(timeframe):
     # timeframes = ['1M', '1w', '3d', '1d', '12h', '8h', '6h', '4h', '2h', '1h', '30m', '15m', '5m', '3m', '1m']
     interval_digit = timeframe[0: len(timeframe) - 1]
     interval_period = timeframe[len(timeframe) - 1: len(timeframe)]
@@ -36,6 +36,6 @@ def get_interval_by_string(timeframe):
 
 
 def get_interval(timeframe, end_time=datetime.utcnow()):
-    interval = get_interval_by_string(timeframe)
+    interval = get_seconds_by_timeframe(timeframe)
     start_time = end_time - timedelta(seconds=interval)
     return start_time, end_time
